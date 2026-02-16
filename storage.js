987065de4d7ac06d3589776a5bc8a4a41bdb11b1
Samkit Mishra <samkitwork2233@gmail.com>
2026-02-17 00:39:16 +0530

    Add save and load functions for profile thresholds
    Added functions to save and load threshold values for profiles.

--- storage.js
+++ storage.js
@@ -3,6 +3,14 @@ function saveProfileData(profile, data) {
 }
 
 function loadProfileData(profile) {
-    let data = localStorage.getItem("postura_" + profile);
+    const data = localStorage.getItem("postura_" + profile);
     return data ? JSON.parse(data) : null;
+}
+
+function saveThreshold(profile, value) {
+    localStorage.setItem("postura_" + profile + "_threshold", value);
+}
+
+function loadThreshold(profile) {
+    return localStorage.getItem("postura_" + profile + "_threshold") || 10;
 }
